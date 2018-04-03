66a97cd6e90c15e49f5bcf99fb94e741a066e824
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-03 13:00:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,11 +16,22 @@ def line
     counter += 1 
   end
    if(katz_deli.length === 0) 
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else 
-  return "The line is currently: #{new_arr}"
+  puts "The line is currently: #{new_arr.join(" ")}"
  end 
  end
  
 
 
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
