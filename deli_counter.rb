ad6e9ef0ffaf1ca11fd0099dea626de0ec10dcb4
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-03 13:12:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 def line(katz_deli)
   new_arr = []
    if(katz_deli.length === 0) 
-    return "The line is empty"
+    puts "The line iscurrently empty."
   else 
     katz_deli.each_with_index do |name, index| 
     new_arr.push("#{index + 1}. #{name}")
