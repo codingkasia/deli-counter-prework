7be459710850348053556d1bfc9d2bd98b8fe150
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-03 13:10:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,16 +20,3 @@ def line(katz_deli)
  end 
  end
  
-
-
-def line(x)
-  line_array = []
-  if x.length == 0
-    puts "The line is currently empty."
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{name}")
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
-  end
-end
