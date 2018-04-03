70f23f789e7d32855b80c3dcc305ca5737486d1e
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-03 13:25:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,6 +29,6 @@ def now_serving(katz_deli)
   if(katz_deli.size ==0)
     puts "There is nobody waiting to be served!"
   else 
-    puts 
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.pus
     
